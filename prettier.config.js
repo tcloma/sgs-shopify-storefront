c8ead79bf663e63e284582e5a406ac9324e457a5
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2023-02-13 19:04:17 -0500

    setup tailwindcss and prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
